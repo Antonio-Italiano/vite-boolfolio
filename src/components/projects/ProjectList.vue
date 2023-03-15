647d6c81ebb259952b453f1a5f44e2432ac3698a
Antonio-Italiano <antoitalia221@gmail.com>
2023-03-15 19:43:35 +0100

    created ProjectList

---
+++ src/components/projects/ProjectList.vue
@@ -0,0 +1,18 @@
+<script>
+import ProjectCard from './ProjectCard.vue';
+export default {
+    name: 'ProjectList',
+    components: { ProjectCard },
+    props: { projects: Array },
+
+};
+</script>
+
+<template>
+    <section id="projects-list">
+        <div v-if="projects.length">
+            <project-card v-for="project in projects" :key="project.id" :project="project"></project-card>
+        </div>
+        <h4 v-else class="text-center">Non ci sono progetti</h4>
+    </section>
+</template>
