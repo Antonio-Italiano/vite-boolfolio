ae27dc566715ce19f7e79102f02a44fdfd5a5ee7
Antonio-Italiano <antoitalia221@gmail.com>
2023-03-15 19:44:40 +0100

    called axios

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <script>
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+import ProjectList from './components/projects/ProjectList.vue';
+const apiBaseUrl = 'http://127.0.0.1:8000/api/';
 export default {
-  name: 'App'
+  name: 'App',
+  components: { AppHeader, ProjectList },
+  data: () => ({
+    projects: []
+  }),
+  methods: {
+    fetchProjects() {
+      axios.get(apiBaseUrl + 'projects').then(res => {
+        this.projects = res.data;
+      })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  },
 
 };
 </script>
 
-<style lang="">
-  
-</style>
-
 <template>
-  <div>
-
-  </div>
+  <app-header></app-header>
+  <main>
+    <project-list :projects="projects"></project-list>
+  </main>
 </template>
+
+<style></style>
