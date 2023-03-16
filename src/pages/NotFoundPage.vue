42bbcd50b6283df6ef0f4517d8675b06eec1caca
Antonio-Italiano <antoitalia221@gmail.com>
2023-03-16 22:06:03 +0100

    created pages/NotFoundPage

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'NotFoundPage',
+
+}
+</script>
+
+<template>
+    <div class="d-flex flex-column align-items-center justify-content-center vh-100">
+        <h2 class="text-center text-white">404 | Pagina Inesistente</h2>
+        <button @click="$router.back()" class="btn btn-secondary my-3">indietro</button>
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
