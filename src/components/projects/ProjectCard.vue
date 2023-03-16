9850635de6e734a6a7f9fedd54de4c53817124c3
Antonio-Italiano <antoitalia221@gmail.com>
2023-03-16 14:22:54 +0100

    added formatDate to ProjectCard

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -3,20 +3,22 @@ export default {
     name: 'ProjectCard',
     props: { project: Object },
     computed: {
-        // formatDate() {
-        //     const date = new Date(this.project.update_at);
-        //     const day = date.getDay();
-        //     const month = date.getMonth();
-        //     const year = date.getYear();
-        //     const hours = date.getHours();
-        //     const minutes = date.getMinutes();
-        //     const seconds = date.getSeconds();
+        formatDate() {
+            const date = new Date(this.project.updated_at);
+            let day = date.getDay();
+            let month = date.getMonth();
+            const year = date.getFullYear();
+            const hours = date.getHours();
+            let minutes = date.getMinutes();
+            let seconds = date.getSeconds();
 
-        //     // if (day < 10) day = "0" + day;
-        //     // if (month < 10) month = "0" + month;
+            if (day < 10) day = "0" + day;
+            if (month < 10) month = "0" + month;
+            if (minutes < 10) minutes = "0" + minutes;
+            if (seconds < 10) seconds = "0" + seconds;
 
-        //     return `${day}/${month}/${year} - ${hours}:${minutes}:${seconds}`;
-        // }
+            return `${day}/${month}/${year} - ${hours}:${minutes}:${seconds}`;
+        }
     }
 }
 </script>
@@ -32,7 +34,7 @@ export default {
         </div>
         <div class="card-footer d-flex justify-content-between">
             <p>{{project.slug}}</p>
-            <p>{{project.updated_at}}</p>
+            <time>{{formatDate}}</time>
         </div>
     </div>
 </template>
